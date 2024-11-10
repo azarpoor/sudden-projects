04b24a414de4769b70fa9f4732ab3c8109435797
Amirhossein Azarpour <holyblackhole@proton.me>
2024-11-10 19:56:45 +0330

    refact: print all charactors ASCII codes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-mod vars;
-
-use std::thread;
+use crossterm::terminal::disable_raw_mode;
+use crossterm::terminal::enable_raw_mode;
+use std::io::{self, Read};
 
 fn main() {
-    vars::add_one(2);
-    vars::pluser();
-
-    static X: [i32; 3] = [1, 2, 3];
-
-    thread::spawn(|| dbg!(&X));
-    thread::spawn(|| dbg!(&X));
+    enable_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+        } else {
+            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+        }
+        if c == 'q' {
+            disable_raw_mode().unwrap();
+            break;
+        }
+    }
 }
